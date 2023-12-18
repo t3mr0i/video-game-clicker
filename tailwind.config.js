0119582eeb03e7654a94b6d4d01e253207c6bee8
t3mr0i <detmers1@uni-bremen.de>
2023-12-18 19:55:58 +0100

    before redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,39 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        cerulean: {
+          DEFAULT: '#227c9d',
+          // Define other shades if necessary
+          600: '#1e7085',
+        },
+        'light-sea-green': {
+          DEFAULT: '#17c3b2',
+          // Define other shades if necessary
+          600: '#159a91',
+        },
+        sunset: {
+          DEFAULT: '#ffcb77',
+          // Define other shades if necessary
+          600: '#e6b766',
+        },
+        'floral-white': {
+          DEFAULT: '#fef9ef',
+          // Define other shades if necessary
+          600: '#e6dfd6',
+        },
+        'light-red': {
+          DEFAULT: '#fe6d73',
+          // Define other shades if necessary
+          600: '#e66268',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      // Add other theme extensions if necessary
     },
   },
   plugins: [],
-}
+};
